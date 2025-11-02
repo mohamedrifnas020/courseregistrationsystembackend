b2837b505e91fb76bf4c6664406f28a8dc89156b
mohamedrifnas020 <mohamedrifnas020@gmail.com>
2025-11-03 00:45:08 +0530

    front url

--- src/main/java/com/example/course/registration/system/controller/coursecontroller.java
+++ src/main/java/com/example/course/registration/system/controller/coursecontroller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:5500")
+@CrossOrigin("https://mohamedrifnas020.github.io/courseregistrationsystemfrontend")
 
 public class coursecontroller {
     @Autowired
