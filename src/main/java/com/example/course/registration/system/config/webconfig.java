0ad07edb67bab329bb4c64347acde628ec4a5e86
mohamedrifnas020 <mohamedrifnas020@gmail.com>
2025-11-03 01:12:12 +0530

    front urll

---
+++ src/main/java/com/example/course/registration/system/config/webconfig.java
@@ -0,0 +1,24 @@
+package com.example.course.registration.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webconfig{
+
+    @Bean
+    public WebMvcConfigurer corsconfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+               registry.addMapping("/**").allowedOrigins(
+                       "https://mohamedrifnas020.github.io/courseregistrationsystemfrontend"
+               ).allowedMethods("GET","POST","PUT","DELETE","OPTIONS").allowedHeaders("*");
+            }
+        };
+    }
+
+
+}
