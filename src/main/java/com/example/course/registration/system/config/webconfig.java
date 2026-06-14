c8fda6578c8d3b5842d383742b923ab9e4e2d45b
mohamedrifnas020 <mohamedrifnas020@gmail.com>
2026-06-15 02:15:38 +0530

    Dockerized application

--- src/main/java/com/example/course/registration/system/config/webconfig.java
+++ src/main/java/com/example/course/registration/system/config/webconfig.java
@@ -16,10 +16,33 @@ public class webconfig{
                registry.addMapping("/**").allowedOrigins(
                        "*"
 
+
                ).allowedMethods("GET","POST","PUT","DELETE","OPTIONS").allowedHeaders("*");
             }
         };
+//
     }
 
 
 }
+//     import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//        @Configuration
+//        public class CorsConfig {
+//
+//            @Bean
+//            public WebMvcConfigurer corsConfigurer() {
+//                return new WebMvcConfigurer() {
+//                    @Override
+//                    public void addCorsMappings(CorsRegistry registry) {
+//                        registry.addMapping("/**")
+//                                .allowedOrigins("*")
+//                                .allowedMethods("*")
+//                                .allowedHeaders("*");
+//                    }
+//                };
+//            }
+//        }
